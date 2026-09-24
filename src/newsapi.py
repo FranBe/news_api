8f643bca6d17f075a3afe2266f52419c9f6e353f
FranBe <franb2289@gmail.com>
2026-09-24 18:44:32 -0300

    Adding python code

---
+++ src/newsapi.py
@@ -0,0 +1,57 @@
+#****************************************************************
+#                           Libraries
+#****************************************************************
+
+import requests
+from dotenv import load_dotenv
+import os
+
+#****************************************************************
+#                       Preconfiguration
+#****************************************************************
+
+# Initializing dotenv
+load_dotenv()
+
+# Loading environment variables
+API_KEY = os.getenv('API_KEY')
+
+# Base URL
+
+base_url = "https://newsapi.org/v2/"
+
+#****************************************************************
+#                       Program
+#****************************************************************
+
+main = "everything"
+country = "us"
+source = "bbc-news"
+subject="el niño"
+date_from="2026-09-20"
+sort_by="popularity"
+rest_url = f"everything?q={subject}&from={date_from}&sortBy={sort_by}&apiKey={API_KEY}"
+
+# Request
+full_url = base_url + rest_url
+
+try:
+    r = requests.get(full_url)
+except requests.exceptions.Timeout:
+    # Maybe set up for a retry, or continue in a retry loop
+    print("Timeout error!")
+except requests.exceptions.TooManyRedirects:
+    # Tell the user their URL was bad and try a different one
+    print("TooManyRedirects!")
+except requests.exceptions.RequestException as e:
+    # catastrophic error. bail.
+    raise SystemExit(e)
+
+
+content = r.json()
+
+for article in content["articles"]:
+    print(article["title"])
+    print("------------------------")
+    print(article["description"])
+    print("***********************")
