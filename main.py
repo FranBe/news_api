1a0ac93406e3c08e75bdf8b22b7e46c93aec56a9
FranBe <franb2289@gmail.com>
2026-09-25 20:49:40 -0300

    Polishing news api

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from src.email import send_email
+from src import newsapi
 
 def main():
-    send_email.send_email("Este es el mensaje")
+
+    newsapi.RetrieveNews()
+    #send_email.send_email("Este es el mensaje")
 
 
 if __name__ == "__main__":
